49bacd93651d60788cd4395f6a4349435fa4e888
Elián Herrera Correa <herreraelian95@gmail.com>
2026-06-16 11:35:07 -0500

    Feat: Clase del 16/06/2026, Elian Herrera

---
+++ Unidad02/00javascript/clase-number.js
@@ -0,0 +1,37 @@
+//TIPOS DE DATOS NUMERICOS
+
+//Enteros y decimal
+const entero = 42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal) 
+
+//Notacion cientifica
+const cientifico = 5e3
+
+//Represetacion de otros contextos
+const infinito = Infinity
+const noEsUnNumero = NaN
+
+//Operaciones Aritmeticas
+
+//Suma, Resta, Multiplicacion, Division
+const suma = 3 + 4
+const resta = 4 - 4
+const multiplicacion = 4 * 7
+const division = 10 / 2
+
+//Modulos de Exponenciacion
+const modulo = 15 % 8
+
+//Resultados de presicion
+const exponenciacion = 0.1 + 0.2
+console.log(exponenciacion)
+console.log(exponenciacion.toFixed(1))
+
+const raizcuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-7)
+const aleatorio = Math.random()
+
+console.log(raizcuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
